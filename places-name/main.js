767ff5361c28a6b5741da8446806ca2cbcba4f64
Vlad <vavilikhin.v@gmail.com>
2020-03-31 11:46:41 +0300

    first commit

---
+++ places-name/main.js
@@ -0,0 +1,18 @@
+AFRAME.registerComponent('rotating-camera', {
+  schema: {
+    target: {type: 'selector'}
+  },
+	
+  tick: function () {
+	var currentRotation = this.el.object3D.rotation;
+	var targetRotation = this.data.target.object3D.rotation;
+	
+	console.log(targetRotation)
+	
+	this.el.setAttribute('rotation', {
+      x: -90,
+      y: THREE.Math.radToDeg(targetRotation.y),
+      z: 0,
+    });
+  }
+});
